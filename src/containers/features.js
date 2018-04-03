ec9807749d6eff7c6e94c90b6275df38f16a4d59
Aldo Torres <atorres6d5@gmail.com>
2018-04-03 10:58:15 -0700

    first commit

---
+++ src/containers/features.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Features extends Component {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+
+}
+
+export default Features;
